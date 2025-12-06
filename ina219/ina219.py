ea74db8a261f8b0fc9a65e1eabcedb5ca7075d38
EvgeniyGT <evgeniy.gt@gmail.com>
2025-12-07 01:53:21 +0200

    deploy

--- ina219/ina219.py
+++ ina219/ina219.py
@@ -1,35 +1,33 @@
-#from smbus2 import SMBus
+from smbus2 import SMBus
 import time
 
-#I2C_BUS = 13  # Попробуем i2c-13 (по твоему выводу)
-#ADDRESS = 0x41
-#
-## Регистр Bus Voltage
-#REG_BUS_VOLTAGE = 0x02
-#REG_CURRENT = 0x04
-#REG_POWER = 0x03
+I2C_BUS = 1
+ADDRESS = 0x41
 
-#def read_word(bus, reg):
-#    data = bus.read_i2c_block_data(ADDRESS, reg, 2)
-#    return (data[0] << 8) | data[1]
+# Регистр Bus Voltage
+REG_BUS_VOLTAGE = 0x02
+REG_CURRENT = 0x04
+REG_POWER = 0x03
+
+def read_word(bus, reg):
+    data = bus.read_i2c_block_data(ADDRESS, reg, 2)
+    return (data[0] << 8) | data[1]
 
 while True:
-    print("HELLO!")
+    try:
+        with SMBus(I2C_BUS) as bus:
+            bus_voltage_raw = read_word(bus, REG_BUS_VOLTAGE)
+            bus_voltage = (bus_voltage_raw >> 3) * 0.004
 
-#    try:
-#        with SMBus(I2C_BUS) as bus:
-#            bus_voltage_raw = read_word(bus, REG_BUS_VOLTAGE)
-#            bus_voltage = (bus_voltage_raw >> 3) * 0.004
-#
-#            power_raw = read_word(bus, REG_POWER)
-#            current_raw = read_word(bus, REG_CURRENT)
-#
-#            print("Load Voltage: {:.3f} V".format(bus_voltage))
-#            print("Current RAW: {}".format(current_raw))
-#            print("Power RAW: {}".format(power_raw))
-#            print("-----")
-#
-#    except Exception as e:
-#        print("Error:", e)
+            power_raw = read_word(bus, REG_POWER)
+            current_raw = read_word(bus, REG_CURRENT)
+
+            print("Load Voltage: {:.3f} V".format(bus_voltage))
+            print("Current RAW: {}".format(current_raw))
+            print("Power RAW: {}".format(power_raw))
+            print("-----")
+
+    except Exception as e:
+        print("Error:", e)
 
     time.sleep(2)
